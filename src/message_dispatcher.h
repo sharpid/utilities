a1868ee8ee7305513c488ce7ad6e213c59dd45cb
wayne seo <sharpncool@gmail.com>
2019-10-01 11:00:28 +0900

    make template return type, add exception when fail dispatch

--- src/message_dispatcher.h
+++ src/message_dispatcher.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <unordered_map>
+#include <stdexcept>
 
-template <typename T_TYPE, typename T_FUNC, typename T_RET, typename ... T_ARGS >
+template <typename T_TYPE, typename T_FUNC, typename ... T_ARGS >
 class MessageDispatcher
 {
 	typedef std::unordered_map<T_TYPE, T_FUNC> MessageMapT;
@@ -10,20 +11,22 @@ public:
 	virtual ~MessageDispatcher() { message_map_.clear(); }
 
 	void RegisterMessage(T_TYPE _type, T_FUNC _func);
+	template<typename T_RET>
 	T_RET DispatchMessage(T_TYPE _type, T_ARGS...);
 
 private:
 	MessageMapT message_map_;
 };
 
-template<typename T_TYPE, typename T_FUNC, typename T_RET, typename ...T_ARGS>
-inline void MessageDispatcher<T_TYPE, T_FUNC, T_RET, T_ARGS...>::RegisterMessage(T_TYPE _type, T_FUNC _func)
+template<typename T_TYPE, typename T_FUNC, typename ...T_ARGS>
+inline void MessageDispatcher<T_TYPE, T_FUNC, T_ARGS...>::RegisterMessage(T_TYPE _type, T_FUNC _func)
 {
 	message_map_.emplace(_type, _func);
 }
 
-template<typename T_TYPE, typename T_FUNC, typename T_RET, typename ...T_ARGS>
-inline T_RET MessageDispatcher<T_TYPE, T_FUNC, T_RET, T_ARGS...>::DispatchMessage(T_TYPE _type, T_ARGS ... _args)
+template<typename T_TYPE, typename T_FUNC, typename ...T_ARGS>
+template<typename T_RET>
+inline T_RET MessageDispatcher<T_TYPE, T_FUNC, T_ARGS...>::DispatchMessage(T_TYPE _type, T_ARGS ... _args)
 {
 	auto it = message_map_.find(_type);
 
@@ -33,7 +36,7 @@ inline T_RET MessageDispatcher<T_TYPE, T_FUNC, T_RET, T_ARGS...>::DispatchMessag
 	}
 	else
 	{
-		return false;
+		throw std::runtime_error("dispatch fail");
 	}
 }
 
