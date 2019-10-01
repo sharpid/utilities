6457b9e21a6add5c44331be938226be1f9a5eab2
wayne seo <sharpncool@gmail.com>
2019-10-01 10:18:36 +0900

    make template return type

--- src/message_dispatcher.h
+++ src/message_dispatcher.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <unordered_map>
 
-template <typename T_TYPE, typename T_FUNC, typename ... T_ARGS >
+template <typename T_TYPE, typename T_FUNC, typename T_RET, typename ... T_ARGS >
 class MessageDispatcher
 {
 	typedef std::unordered_map<T_TYPE, T_FUNC> MessageMapT;
@@ -10,20 +10,20 @@ public:
 	virtual ~MessageDispatcher() { message_map_.clear(); }
 
 	void RegisterMessage(T_TYPE _type, T_FUNC _func);
-	bool DispatchMessage(T_TYPE _type, T_ARGS...);
+	T_RET DispatchMessage(T_TYPE _type, T_ARGS...);
 
 private:
 	MessageMapT message_map_;
 };
 
-template<typename T_TYPE, typename T_FUNC, typename ...T_ARGS>
-inline void MessageDispatcher<T_TYPE, T_FUNC, T_ARGS...>::RegisterMessage(T_TYPE _type, T_FUNC _func)
+template<typename T_TYPE, typename T_FUNC, typename T_RET, typename ...T_ARGS>
+inline void MessageDispatcher<T_TYPE, T_FUNC, T_RET, T_ARGS...>::RegisterMessage(T_TYPE _type, T_FUNC _func)
 {
 	message_map_.emplace(_type, _func);
 }
 
-template<typename T_TYPE, typename T_FUNC, typename ...T_ARGS>
-inline bool MessageDispatcher<T_TYPE, T_FUNC, T_ARGS...>::DispatchMessage(T_TYPE _type, T_ARGS ... _args)
+template<typename T_TYPE, typename T_FUNC, typename T_RET, typename ...T_ARGS>
+inline T_RET MessageDispatcher<T_TYPE, T_FUNC, T_RET, T_ARGS...>::DispatchMessage(T_TYPE _type, T_ARGS ... _args)
 {
 	auto it = message_map_.find(_type);
 
