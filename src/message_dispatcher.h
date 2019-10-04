0762d15a8fe780f056e56d7badf8449ea0df211b
wayne seo <sharpncool@gmail.com>
2019-10-04 11:20:06 +0900

    fix, remove reference

--- src/message_dispatcher.h
+++ src/message_dispatcher.h
@@ -11,20 +11,20 @@ public:
 	MessageDispatcher() = default;
 	virtual ~MessageDispatcher() = default;
 
-	void	RegisterMessage(const T_TYPE& _type, const T_FUNC& _func);
-	T_RET	DispatchMessage(const T_TYPE& _type, const T_ARGS& ... _args) const;
+	void	RegisterMessage(T_TYPE _type, T_FUNC _func);
+	T_RET	DispatchMessage(T_TYPE _type, T_ARGS ... _args) const;
 
 private:
 	MessageMapT message_map_;
 };
 
 template <typename T_TYPE, typename T_RET, typename ... T_ARGS >
-inline void MessageDispatcher<T_TYPE, T_RET, T_ARGS...>::RegisterMessage(const T_TYPE& _type, const T_FUNC& _func) {
+inline void MessageDispatcher<T_TYPE, T_RET, T_ARGS...>::RegisterMessage(T_TYPE _type, T_FUNC _func) {
 	message_map_.emplace(_type, _func);
 }
 
 template<typename T_TYPE, typename T_RET, typename ...T_ARGS>
-inline T_RET MessageDispatcher<T_TYPE, T_RET, T_ARGS...>::DispatchMessage(const T_TYPE& _type, const T_ARGS& ... _args) const {
+inline T_RET MessageDispatcher<T_TYPE, T_RET, T_ARGS...>::DispatchMessage(T_TYPE& _type, T_ARGS ... _args) const {
 	auto it = message_map_.find(_type);
 
 	if (it != message_map_.end()) {
