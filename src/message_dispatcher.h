f60352c951ca07c138b77514a2f71a49665dfac8
wayne seo <sharpncool@gmail.com>
2019-10-04 11:12:57 +0900

    fix, cant guess template type of T_RET

--- src/message_dispatcher.h
+++ src/message_dispatcher.h
@@ -1,41 +1,36 @@
 #pragma once
 #include <unordered_map>
 #include <stdexcept>
+#include <functional>
 
-template <typename T_TYPE, typename T_FUNC, typename ... T_ARGS >
-class MessageDispatcher
-{
-	typedef std::unordered_map<T_TYPE, T_FUNC> MessageMapT;
+template <typename T_TYPE, typename T_RET, typename ... T_ARGS >
+class MessageDispatcher {
+	using T_FUNC = std::function<T_RET(T_ARGS...)>;
+	using MessageMapT = std::unordered_map<T_TYPE, T_FUNC>;
 public:
 	MessageDispatcher() = default;
-	virtual ~MessageDispatcher() { message_map_.clear(); }
+	virtual ~MessageDispatcher() = default;
 
-	void RegisterMessage(T_TYPE _type, T_FUNC _func);
-	template<typename T_RET>
-	T_RET DispatchMessage(T_TYPE _type, T_ARGS...);
+	void	RegisterMessage(const T_TYPE& _type, const T_FUNC& _func);
+	T_RET	DispatchMessage(const T_TYPE& _type, const T_ARGS& ... _args) const;
 
 private:
 	MessageMapT message_map_;
 };
 
-template<typename T_TYPE, typename T_FUNC, typename ...T_ARGS>
-inline void MessageDispatcher<T_TYPE, T_FUNC, T_ARGS...>::RegisterMessage(T_TYPE _type, T_FUNC _func)
-{
+template <typename T_TYPE, typename T_RET, typename ... T_ARGS >
+inline void MessageDispatcher<T_TYPE, T_RET, T_ARGS...>::RegisterMessage(const T_TYPE& _type, const T_FUNC& _func) {
 	message_map_.emplace(_type, _func);
 }
 
-template<typename T_TYPE, typename T_FUNC, typename ...T_ARGS>
-template<typename T_RET>
-inline T_RET MessageDispatcher<T_TYPE, T_FUNC, T_ARGS...>::DispatchMessage(T_TYPE _type, T_ARGS ... _args)
-{
+template<typename T_TYPE, typename T_RET, typename ...T_ARGS>
+inline T_RET MessageDispatcher<T_TYPE, T_RET, T_ARGS...>::DispatchMessage(const T_TYPE& _type, const T_ARGS& ... _args) const {
 	auto it = message_map_.find(_type);
 
-	if (it != message_map_.end())
-	{
+	if (it != message_map_.end()) {
 		return ( *it ).second(_args ...);
 	}
-	else
-	{
+	else {
 		throw std::runtime_error("dispatch fail");
 	}
 }
