ca95529f373131367ed466e92108894388a5bb2d
wayne seo <sharpncool@gmail.com>
2020-01-31 12:32:16 +0900

    add simple_random_generator.h

---
+++ src/simple_random_generator.h
@@ -0,0 +1,58 @@
+#pragma once
+// simple static random number generator
+#include <boost/serialization/singleton.hpp>
+#include <random>
+#include <type_traits>
+
+class SimpleRandomGenerator : public boost::serialization::singleton<SimpleRandomGenerator> {
+	friend class boost::serialization::singleton<SimpleRandomGenerator>;
+	using engine_type = std::mt19937;
+
+   public:
+	SimpleRandomGenerator() : engine_(seed_()) {}
+
+   public:
+	template <typename T>
+	T Generate(T _min = 1, T _max = 10000);
+
+   private:
+	template <typename T, typename E, bool = std::is_integral<T>::value, bool = std::is_floating_point<T>::value>
+	struct __distributor__;
+
+   private:
+	std::random_device seed_;
+	engine_type engine_;
+};
+
+template <typename T, typename E>
+struct SimpleRandomGenerator::__distributor__<T, E, true, false> {
+	// for integral number types
+	inline T operator()(E& _engine, const T& _min, const T& _max) {
+		auto dist = std::uniform_int_distribution<T>(_min, _max);
+		return dist(_engine);
+	}
+};
+
+template <typename T, typename E>
+struct SimpleRandomGenerator::__distributor__<T, E, false, true> {
+	// for floating point number types
+	inline T operator()(E& _engine, const T& _min, const T& _max) {
+		auto dist = std::uniform_real_distribution<T>(_min, _max);
+		return dist(_engine);
+	}
+};
+
+template <typename T>
+inline T SimpleRandomGenerator::Generate(T _min, T _max) {
+	if (_min == _max)
+		return _min;
+
+	// prevent exception
+	if (_min > _max)
+		std::swap(_min, _max);
+
+	return __distributor__<T, engine_type>()(engine_, _min, _max);
+}
+
+#define RAND(MIN, MAX) SimpleRandomGenerator::get_mutable_instance().Generate(MIN, MAX)
+
