bdaa2cabbbd2789ff123b5bbb69024bf761189b8
wayne seo <sharpncool@gmail.com>
2020-05-14 10:19:25 +0900

    remove useless template argument and using const reference of argument

--- src/simple_random_generator.h
+++ src/simple_random_generator.h
@@ -8,46 +8,45 @@ class SimpleRandomGenerator {
 
    public:
 	SimpleRandomGenerator() : engine_(std::random_device()()) {}
-	//SimpleRandomGenerator() : engine_(std::random_device("/dev/random")()) {}
+	// SimpleRandomGenerator() : engine_(std::random_device("/dev/random")()) {}
 
    public:
 	template <typename T>
-	T Generate(T _min = 1, T _max = 10000);
+	T Generate(const T& _min = 1, const T& _max = 10000);
 
    private:
-	template <typename T, typename E, bool = std::is_integral<T>::value, bool = std::is_floating_point<T>::value>
+	template <typename T, bool = std::is_integral<T>::value, bool = std::is_floating_point<T>::value>
 	struct __distributor__;
 
    private:
 	engine_type engine_;
 };
 
-template <typename T, typename E>
-struct SimpleRandomGenerator::__distributor__<T, E, true, false> {
-	// for integral number types
-	inline T operator()(E& _engine, const T& _min, const T& _max) {
+// integral number types
+template <typename T>
+struct SimpleRandomGenerator::__distributor__<T, true, false> {
+	inline T operator()(engine_type& _engine, const T& _min, const T& _max) {
 		auto dist = std::uniform_int_distribution<T>(_min, _max);
 		return dist(_engine);
 	}
 };
 
-template <typename T, typename E>
-struct SimpleRandomGenerator::__distributor__<T, E, false, true> {
-	// for floating point number types
-	inline T operator()(E& _engine, const T& _min, const T& _max) {
+// floating point number types
+template <typename T>
+struct SimpleRandomGenerator::__distributor__<T, false, true> {
+	inline T operator()(engine_type& _engine, const T& _min, const T& _max) {
 		auto dist = std::uniform_real_distribution<T>(_min, _max);
 		return dist(_engine);
 	}
 };
 
 template <typename T>
-inline T SimpleRandomGenerator::Generate(T _min, T _max) {
+inline T SimpleRandomGenerator::Generate(const T& _min, const T& _max) {
 	if (_min == _max)
 		return _min;
 
-	// prevent exception
 	if (_min > _max)
-		std::swap(_min, _max);
-
-	return __distributor__<T, engine_type>()(engine_, _min, _max);
+		return __distributor__<T>()(engine_, _max, _min);
+	else
+		return __distributor__<T>()(engine_, _min, _max);
 }
